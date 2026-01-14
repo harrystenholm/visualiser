eb2259836ec1e488a7f2ec7283951d7af70683ac
hazza <harrystenholm@yahoo.com>
2026-01-14 12:36:40 +0000

    tkinter updates

--- GUI/gui.py
+++ GUI/gui.py
@@ -12,34 +12,37 @@ class feetToMeters:
         mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 
         self.feet = StringVar()
-        feet_entry = ttk.Entry(mainframe, width=7, textvariable=self.feet)
-        feet_entry.grid(column=2,row=1, sticky=(W,E))
+        ttk.Button(mainframe, text="Launch Program", command = self.run).grid(column=2, row=2)
         self.meters = StringVar()
 
-        ttk.Label(mainframe, textvariable=self.meters).grid(column=2, row=2, sticky=(W,E))
-
-        ttk.Button(mainframe, text="Calculate", command=self.calculate).grid(column=3, row=3, sticky=W)
-
-        ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-        ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-        ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
-
+        l =ttk.Label(mainframe, text="Starting...")
+        l.grid(column=2, row=3)
+        l.bind('<Enter>', lambda e: l.configure(text='Moved mouse inside'))
+        l.bind('<Leave>', lambda e: l.configure(text='Moved mouse outside'))
+        l.bind('<ButtonPress-1>', lambda e: l.configure(text='Clicked left mouse button'))
+        l.bind('<3>', lambda e: l.configure(text='Clicked third mouse button'))
+        l.bind('<Double-1>', lambda e: l.configure(text='Double clicked'))
+        l.bind('<B3-Motion>', lambda e: l.configure(text=f'third button drag to {e.x},{e.y}'))
+        
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
+
         mainframe.columnconfigure(2, weight=1)
         for child in mainframe.winfo_children():
             child.grid_configure(padx=5, pady=5)
 
-        feet_entry.focus()
-        root.bind("<Return>", self.calculate)
+        root.bind("<Return>", self.run)
 
         
-    def calculate(self, *args):
-        try: 
-            value = float(self.feet.get())
-            self.meters.set(round(0.3048 * value, 4))
-        except ValueError:
-            pass
+    def run(self):
+        pass
+    
+    # def calculate(self, *args):
+    #     try: 
+    #         value = float(self.feet.get())
+    #         self.meters.set(round(0.3048 * value, 4))
+    #     except ValueError:
+    #         pass
 
 root = Tk()
 feetToMeters(root)
